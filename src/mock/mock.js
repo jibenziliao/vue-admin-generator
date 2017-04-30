c40b712afbf3e9af517774c0daa9598fa9fc7c8b
jibenziliao <1501718947@qq.com>
2017-04-30 17:08:28 +0800

    Add MockAdapter
    Simulate Http request

---
+++ src/mock/mock.js
@@ -0,0 +1,71 @@
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import { LoginUsers } from './data/exampleData'
+
+export default {
+  /**
+   * mock bootstrap
+   */
+  bootstrap () {
+    let mock = new MockAdapter(axios)
+
+    // mock success request
+    mock.onGet('/success').reply(200, {
+      msg: 'success'
+    })
+
+    // mock error request
+    mock.onGet('/error').reply(500, {
+      msg: 'failure'
+    })
+
+    // 进入登录页面，获取cookie
+    mock.onPost('/account/login').reply(config => {
+      console.log(config)
+    })
+
+    // 登录
+    mock.onPost('/account/doLogin').reply(config => {
+      let { username, password } = JSON.parse(config.data)
+      return new Promise((resolve, reject) => {
+        let user = null
+        setTimeout(() => {
+          let hasUser = LoginUsers.some(u => {
+            if (u.username === username && u.password === password) {
+              user = JSON.parse(JSON.stringify(u))
+              user.password = undefined
+              return true
+            }
+          })
+          if (hasUser) {
+            resolve([200, { code: 200, msg: '请求成功', user }])
+          } else {
+            resolve([200, { code: 500, msg: '账号或密码错误' }])
+          }
+        }, 1000)
+      })
+    })
+
+    // 登录Test
+    mock.onPost('/account/doLoginTest').reply(config => {
+      let { username, password } = JSON.parse(config.data)
+      return new Promise((resolve, reject) => {
+        let user = null
+        setTimeout(() => {
+          let hasUser = LoginUsers.some(u => {
+            if (u.username === username && u.password === password) {
+              user = JSON.parse(JSON.stringify(u))
+              user.password = undefined
+              return true
+            }
+          })
+          if (hasUser) {
+            resolve([200, { code: 200, msg: '请求成功', user }])
+          } else {
+            resolve([200, { code: 500, msg: '账号或密码错误' }])
+          }
+        }, 1000)
+      })
+    })
+  }
+}
